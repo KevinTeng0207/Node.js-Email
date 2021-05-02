c8f393c3ba3d26037daae8a00d627b890684aed5
kevin <johndoe@example.com>
2021-05-03 00:53:45 +0800

    first project

---
+++ ReadMe.md
@@ -0,0 +1,24 @@
+# Express JS API Mailer
+
+## 安裝套件
+```
+npm install express
+npm install nodemailer
+```
+## DB config
+
+### **檔案名稱：dbconfig.json**
+
+```json
+{
+    "Gmail": {
+        "user": "email",
+        "pass": "password"
+    }
+}
+```
+## 執行
+
+```
+node email.js
+```
---
+++ email.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const nodemailer = require('nodemailer');
+const mailconfig = require('./email.json');
+
+app.use(express.json()); // post解析body
+app.get('/', (req, res) => { // '/'是指專案的根目錄路徑
+    res.send('Travel API!');
+});
+
+var transporter = nodemailer.createTransport({
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    service: 'gmail',
+    auth: {
+        user: mailconfig.Gmail.user,
+        pass: mailconfig.Gmail.pass
+    }
+});
+
+app.post('/send', (req, res) => { // '/'是指專案的根目錄路徑
+
+    var mailOptions = {
+        from: mailconfig.Gmail.user,
+        to: req.body,
+        subject: 'Sending Email using Node.js',
+        text: 'Nice to meet you！'
+    };
+
+    try {
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+        res.send('Send success!!!');
+    }
+    catch (error) {
+        res.send('Send fail!!!');
+    }
+
+});
+
+const port = process.env.PORT || 5000; // 修改port, For example url: localhost:5000
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+});
